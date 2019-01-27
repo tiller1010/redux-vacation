3af21c0466cd34406231bdffd35921770ddb378a
Tyler Trout <tiller1010@gmail.com>
2019-01-27 17:45:41 -0500

    trying payload to make distinct actions

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import './vacationStyles.css';
 
 const unbooked='unbooked';
 const booked='booked';
+const unbook='unbook'
 const book='book';
 
 const initialState={
-  flightStatus: '',
+  flightStatus: unbooked,
   hotelStatus: unbooked
 }
 
-const bookActionCreator=()=>{
-  return {type:book}
+const bookActionCreator=(payload)=>{
+  return {type:book, payload: payload}
+}
+
+const unBookActionCreator=(payload)=>{
+  return {type:unbook, payload: payload}
 }
 
 const reducer=(state=initialState,action)=>{
   switch(action.type){
     case book:
-      return {bookStatus:booked}
+      return {Status: booked}
+    case unbook:
+      return {Status: unbooked}
     default:
       return state;
   }
@@ -32,13 +39,19 @@ store.subscribe(()=>{
 });
 
 const mapStateToProps=(state)=>{
-  return {bookStatus:state.bookStatus}
+  return {
+    flightStatus: state.flightStatus,
+    hotelStatus: state.hotelStatus
+  }
 }
 
 const mapDispatchToProps=(dispatch)=>{
   return {
-    book:function(){
-      dispatch(bookActionCreator())
+    book:function(payload){
+      dispatch(bookActionCreator(payload))
+    },
+    unbook:function(payload){
+      dispatch(unBookActionCreator(payload))
     }
   }
 }
@@ -59,6 +72,7 @@ const Booker = connect(mapStateToProps,mapDispatchToProps)(BookerButton);
 const App=()=>{
   return(
     <Provider store={store}>
+      <Booker img="https://www.gannett-cdn.com/presto/2018/12/04/PLOU/e1a042e7-402a-413e-913d-c16e0d0b115f-GettyImages-912360406.jpg?width=534&height=401&fit=bounds&auto=webp"/>
       <Booker img="http://ihg.scene7.com/is/image/ihg/holiday-inn-the-colony-4629618286-4x3"/>
     </Provider>
   )
