e67f192e1138092e18bba133d55303c66197b868
Tyler <tiller1010@gmail.com>
2019-02-05 16:22:34 -0500

    Updates

--- precache-manifest.4cd82842e1339ecd417cd2d12d5671b2.js
+++ precache-manifest.4cd82842e1339ecd417cd2d12d5671b2.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/redux-vacation/static/js/runtime~main.4a6edf91.js"
   },
   {
-    "revision": "5d08bbe1bff0aa465620",
-    "url": "/redux-vacation/static/js/main.5d08bbe1.chunk.js"
+    "revision": "fbeb672dedf4acc58977",
+    "url": "/redux-vacation/static/js/main.fbeb672d.chunk.js"
   },
   {
     "revision": "844e191388bd513fb7db",
     "url": "/redux-vacation/static/js/1.844e1913.chunk.js"
   },
   {
-    "revision": "5d08bbe1bff0aa465620",
-    "url": "/redux-vacation/static/css/main.f5d7c984.chunk.css"
+    "revision": "fbeb672dedf4acc58977",
+    "url": "/redux-vacation/static/css/main.8c8f1fa6.chunk.css"
   },
   {
-    "revision": "c9d92be199406b2e7758978c6369b40d",
+    "revision": "e623b70ebbcb88c5c8841dabea387807",
     "url": "/redux-vacation/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/redux-vacation/precache-manifest.51b76c824b123696fd67d7148a818937.js"
+  "/redux-vacation/precache-manifest.4cd82842e1339ecd417cd2d12d5671b2.js"
 );
 
 workbox.clientsClaim();
